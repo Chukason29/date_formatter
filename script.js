d98e33e88d339a33fd809a158b4dae61d3c83007
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-11 23:08:03 +0100

    introduced the template literals

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const day = date.getDate()
 const month = date.getMonth() + 1
 const year = date.getFullYear()
 const hours = date.getHours()
+const minutes = date.getMinutes()
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate
