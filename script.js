0e4d7300728afa65131f71f2ae1fb6224e48b6f5
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-12 09:41:51 +0100

    added a switch statement for checking options

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const hours = date.getHours()
 const minutes = date.getMinutes()
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate
+
+dateOptionsSelectElement.addEventListener("change", ()=>{
+    switch (dateOptionsSelectElement.value) {
+        case "yyyy-mm-dd":
+            currentDateParagraph.textContent = formattedDate
+            break;
+    
+        default:
+            break;
+    }
+})
